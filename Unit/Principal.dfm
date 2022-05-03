f63785c51d51a2bca57d2e700ab77de3e6c84809
designarttdev@gmail.com <designarttdev>
2022-05-03 11:46:58 -0300

    Implementação de Validação
    Inserir opção de escolher o tipo de select e validei para ser apenas o insert por enquanto.

--- Unit/Principal.dfm
+++ Unit/Principal.dfm
@@ -27,31 +27,58 @@ object frPrincipal: TfrPrincipal
     object Memo1: TMemo
       AlignWithMargins = True
       Left = 11
-      Top = 11
+      Top = 67
       Width = 512
-      Height = 658
+      Height = 602
       Margins.Left = 10
       Margins.Top = 10
       Margins.Right = 10
       Margins.Bottom = 10
       Align = alLeft
+      CharCase = ecUpperCase
       ScrollBars = ssVertical
       TabOrder = 0
     end
     object Memo2: TMemo
       AlignWithMargins = True
       Left = 540
-      Top = 11
+      Top = 67
       Width = 512
-      Height = 658
+      Height = 602
       Margins.Left = 10
       Margins.Top = 10
       Margins.Right = 10
       Margins.Bottom = 10
       Align = alRight
+      CharCase = ecUpperCase
       ReadOnly = True
       ScrollBars = ssVertical
       TabOrder = 1
+    end
+    object pnlParametros: TPanel
+      Left = 1
+      Top = 1
+      Width = 1061
+      Height = 56
+      Align = alTop
+      TabOrder = 2
+      object rgTipo: TRadioGroup
+        AlignWithMargins = True
+        Left = 31
+        Top = 4
+        Width = 491
+        Height = 48
+        Margins.Left = 30
+        Align = alLeft
+        Caption = 'Selecione o tipo'
+        Columns = 3
+        ItemIndex = 1
+        Items.Strings = (
+          'SELECT'
+          'INSERT'
+          'UPDATE')
+        TabOrder = 0
+      end
     end
   end
   object pnlFooter: TPanel
--- Unit/Principal.pas
+++ Unit/Principal.pas
@@ -14,6 +14,8 @@ type
     pnlFooter: TPanel;
     btnRemoverFormatacao: TButton;
     btnFormatar: TButton;
+    pnlParametros: TPanel;
+    rgTipo: TRadioGroup;
     procedure btnFormatarClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
   private
@@ -35,37 +37,48 @@ var
   i, x : Integer;
   vRecomeca : Boolean;
 begin
-  memo2.lines.clear;
-  vAux := Trim(Memo1.Lines.Text.Replace(#13#10, ''));
-  vAux := Trim(vAux.Replace(')', sLineBreak + ') '));
-  vAux := Trim(vAux.Replace('(', '(' + sLineBreak));
-  vAux := Trim(vAux.Replace(',', ',' + sLineBreak));
-  vAux := Trim(vAux.Replace(sLineBreak + ' ', '' + sLineBreak));
-  memo2.Lines.Add(Trim(vAux));
-  vRecomeca := False;
-  x := 000;
-  for i := 0 to Memo2.Lines.Count -1 do
+  if Trim(Memo1.Lines.Text) = '' then
   begin
-    if (not (Pos('(', memo2.Lines.Strings[i]) <> 0) and not(Pos(')', memo2.Lines.Strings[i]) <> 0)) then
-    begin
-      vLinha := vLinha + '{' + FormatFloat('000',x) + '}  ''' + memo2.Lines.Strings[i] + ' '' + ' + sLineBreak;
-    end
-    else if (Pos('VALUES', memo2.Lines.Strings[i]) <> 0) then
-    begin
-      x := 000;
-      vLinha := vLinha + sLineBreak + '{' + FormatFloat('000',x) + '}  ''' + memo2.Lines.Strings[i] + ' '' + ' + sLineBreak;
-      vRecomeca := True;
-    end
-    else
-      vLinha := vLinha + '{' + FormatFloat('000',x) + '}  ''' + memo2.Lines.Strings[i] + ' '' + ' + sLineBreak;
-    x := X + 1;
+    Application.MessageBox(PChar('Campo de origem vazio, favor preencher antes de continuar!'), 'Mais Soluções', MB_OK + MB_ICONSTOP + MB_TOPMOST);
+    Memo1.SetFocus;
+    Exit;
   end;
+  if rgTipo.Items[rgTipo.ItemIndex] = UpperCase('insert') then
+  begin
+    memo2.lines.clear;
+    if Copy(Trim(memo1.Lines.Text), 1, 6) <> UpperCase('insert') then
+      exit;
+    vAux := Trim(Memo1.Lines.Text.Replace(#13#10, '')); // Retira o enter da string
+    vAux := Trim(vAux.Replace(')', sLineBreak + ') ')); // Dá Enter quando encontrar o ")"
+    vAux := Trim(vAux.Replace('(', '(' + sLineBreak));  // Da enter depois do "("
+    vAux := Trim(vAux.Replace(',', ',' + sLineBreak));  // Insere Enter depois da Vírgula
+    vAux := Trim(vAux.Replace(sLineBreak + ' ', '' + sLineBreak)); // Retira o enter e esaço e apenas coloca o Enter
+    memo2.Lines.Add(Trim(vAux));
+    vRecomeca := False;
+    x := 000;
+    for i := 0 to Memo2.Lines.Count -1 do
+    begin
+      if (not (Pos('(', memo2.Lines.Strings[i]) <> 0) and not(Pos(')', memo2.Lines.Strings[i]) <> 0)) then
+      begin
+        vLinha := vLinha + '{' + FormatFloat('000',x) + '}  ''' + memo2.Lines.Strings[i] + ' '' + ' + sLineBreak;
+      end
+      else if (Pos('VALUES', memo2.Lines.Strings[i]) <> 0) then
+      begin
+        x := 000;
+        vLinha := vLinha + sLineBreak + '{' + FormatFloat('000',x) + '}  ''' + memo2.Lines.Strings[i] + ' '' + ' + sLineBreak;
+        vRecomeca := True;
+      end
+      else
+        vLinha := vLinha + '{' + FormatFloat('000',x) + '}  ''' + memo2.Lines.Strings[i] + ' '' + ' + sLineBreak;
+      x := X + 1;
+    end;
 
-  memo2.Lines.Clear;
-  memo2.Lines.Add(Trim(Copy(Trim(vLinha), 1, Length(Trim(vLinha)) - 1)) + ';');
-  Memo2.SelectAll;
-  memo2.CopyToClipboard;
-  ShowMessage('Copiado!');
+    memo2.Lines.Clear;
+    memo2.Lines.Add(Trim(Copy(Trim(vLinha), 1, Length(Trim(vLinha)) - 1)) + ';');
+    Memo2.SelectAll;
+    memo2.CopyToClipboard;
+    ShowMessage('Copiado!');
+  end;
 end;
 
 procedure TfrPrincipal.FormShow(Sender: TObject);
